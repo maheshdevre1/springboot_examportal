76abc7ab3e0b84254f15ffe20859ac7012e11b06
maheshdevre1 <maheshdevre1997@gmail.com>
2025-10-30 01:00:43 +0530

    added field in demo'

--- src/main/java/com/exam/model/Demo.java
+++ src/main/java/com/exam/model/Demo.java
@@ -1,5 +1,30 @@
 package com.exam.model;
 
 public class Demo {
+	
+	private String name;
+	private String salary;
+	private String dept;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSalary() {
+		return salary;
+	}
+	public void setSalary(String salary) {
+		this.salary = salary;
+	}
+	public String getDept() {
+		return dept;
+	}
+	public void setDept(String dept) {
+		this.dept = dept;
+	}
+	
+	
 
 }
